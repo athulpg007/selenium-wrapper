fea9c67bd22928137f010fdbb58bc06328241a80
Athul Girija <athulpg007@gmail.com>
2026-02-22 15:36:03 -0700

    formatting

--- tests/test_samples/test_read_table.py
+++ tests/test_samples/test_read_table.py
@@ -4,7 +4,7 @@ from elements import read_table
 
 
 class TestReadTable:
-	def test_read_table(self, browser):
-		browser.navigate("https://practicetestautomation.com/practice-test-table/")
-		browser.click(read_table.lang_java)
-		time.sleep(3)
+    def test_read_table(self, browser):
+        browser.navigate("https://practicetestautomation.com/practice-test-table/")
+        browser.click(read_table.lang_java)
+        time.sleep(3)
