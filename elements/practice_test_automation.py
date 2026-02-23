32fc8d762da2451b976159aedfbe8269da53ffab
Athul Girija <athulpg007@gmail.com>
2026-02-22 23:10:00 -0700

    Add more tests

--- elements/practice_test_automation.py
+++ elements/practice_test_automation.py
@@ -13,4 +13,12 @@ level_advanced = Element("//*[@value='Advanced']").by_xpath()
 enrollment_dropdown = Element("dropdown-button").by_cls()
 choice_10000 = Element("//*[@data-value='10000']").by_xpath()
 
+sort_dropdown = Element("sortBy").by_id()
+sort_enrollment = Element("//*[@value='col_enroll']").by_xpath()
+sort_course_name = Element("//*[@value='col_course']").by_xpath()
+sort_language = Element("//*[@value='col_lang']").by_xpath()
+
+reset_filters = Element("resetFilters").by_id()
+
 table = Element("courses_table").by_id()
+no_results = Element("noData").by_id()
--- tests/test_samples/test_practice_automation.py
+++ tests/test_samples/test_practice_automation.py
@@ -44,3 +44,43 @@ class TestReadTable:
 		assert set(result["Level"]) == {"Beginner"}
 		for enrollment in result["Enrollments"]:
 			assert int(enrollment) >= 10000
+
+	def test_read_table_no_results(self, browser):
+		browser.navigate("https://practicetestautomation.com/practice-test-table/")
+		# a combination of filters that results in no courses found
+		browser.click(practice_test_automation.lang_python)
+		browser.click(practice_test_automation.level_beginner)
+		browser.wait_for_element(practice_test_automation.no_results)
+		text = browser.get_element_text(practice_test_automation.no_results)
+		assert text == "No matching courses."
+
+	def test_read_table_reset_filters(self, browser):
+		browser.navigate("https://practicetestautomation.com/practice-test-table/")
+		browser.click(practice_test_automation.lang_python)
+		browser.wait_for_element(practice_test_automation.reset_filters)
+		browser.click(practice_test_automation.reset_filters)
+		browser.wait_for_element(practice_test_automation.table)
+
+	def test_read_table_sort_enrollment(self, browser):
+		browser.navigate("https://practicetestautomation.com/practice-test-table/")
+		browser.use_dropdown(practice_test_automation.sort_dropdown, practice_test_automation.sort_enrollment)
+		browser.wait_for_element(practice_test_automation.table)
+		result = browser.read_table(practice_test_automation.table)
+		enrollments = [int(e) for e in result["Enrollments"]]
+		assert enrollments == sorted(enrollments)
+
+	def test_read_table_sort_course_name(self, browser):
+		browser.navigate("https://practicetestautomation.com/practice-test-table/")
+		browser.use_dropdown(practice_test_automation.sort_dropdown, practice_test_automation.sort_course_name)
+		browser.wait_for_element(practice_test_automation.table)
+		result = browser.read_table(practice_test_automation.table)
+		course_names = result["Course Name"]
+		assert course_names == sorted(course_names)
+
+	def test_read_table_sort_language(self, browser):
+		browser.navigate("https://practicetestautomation.com/practice-test-table/")
+		browser.use_dropdown(practice_test_automation.sort_dropdown, practice_test_automation.sort_language)
+		browser.wait_for_element(practice_test_automation.table)
+		result = browser.read_table(practice_test_automation.table)
+		languages = result["Language"]
+		assert languages == sorted(languages)
