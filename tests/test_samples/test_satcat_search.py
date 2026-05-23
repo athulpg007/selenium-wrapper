b47ffc2d63a91ea76e82d2e0fbdaa79c08e90def
Athul Pradeepkumar Girija <athulpg007@gmail.com>
2026-05-22 19:33:19 -0600

    fix satcat cookies popup (#51)

--- tests/test_samples/test_satcat_search.py
+++ tests/test_samples/test_satcat_search.py
@@ -5,6 +5,8 @@ from elements.satcat import dynamic_search_result
 class TestSearchWithDropdown:
 	def test_search_with_dropdown(self, browser):
 		browser.navigate("https://www.satcat.com/")
+		browser.wait_for_element(satcat.reject_cookies)
+		browser.click(satcat.reject_cookies)
 		browser.wait_for_element(satcat.search_input)
 		norad_id: str = "27386"
 		browser.search_with_dropdown(satcat.search_input, norad_id, dynamic_search_result(norad_id))
